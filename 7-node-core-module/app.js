ebbfc5c74ce38554c061bf38c21e94090157aa03
Adam Abiyu <advmabiyu@gmail.com>
2025-11-20 14:09:57 +0700

    belajar core module readline sama filesystem hehe

--- 7-node-core-module/app.js
+++ 7-node-core-module/app.js
@@ -1,15 +1,41 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-// menuliskan string ke file (synchronous)
 // try{
-//     fs.writeFileSync('data/text.txt', 'hello world dengan synchronous');
+
+//     fs.writeFileSync('data/text.txt', 'ini make write synchronous');
 // } catch(e){
 //     console.log(e)
 // }
 
-// menuliskan string ke file (asynchronous)
-// fs.writeFile('data/text.txt', 'Hello World dengan asynchronous', (e) => {
-//     console.log(e);
+// fs.writeFile('data/text.txt', 'ini write make asynchronous', (e)=> {
+//     console.log(e)
 // })
 
-// membaca isi file (synchronous)
+// const data = fs.readFileSync('data/text.txt', 'utf-8');
+// console.log(data);
+
+// fs.readFile('data/text.txt', 'utf-8', (e, data) => {
+//     if(e) throw e;
+//     console.log(data)
+// })
+const fs = require('fs');
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+rl.question('nama kamu? ', (nama) => {
+    rl.question('rumah? ', (rumah) => {
+        const contact = {nama, rumah};
+        const file = fs.readFileSync('data/contacts.json', 'utf-8');
+        const contacts = JSON.parse(file);
+
+        contacts.push(contact)
+
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+        console.log('makasi dah diisi hehe')
+        rl.close()
+    })
+})
