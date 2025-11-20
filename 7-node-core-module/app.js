a3722356534e6464ac01f4da823f972f110ef1c3
Adam Abiyu <advmabiyu@gmail.com>
2025-11-20 16:49:12 +0700

    nambahin episode 8 dan edit episode 7

--- 7-node-core-module/app.js
+++ 7-node-core-module/app.js
@@ -19,8 +19,8 @@
 //     console.log(data)
 // })
 const fs = require('fs');
-
 const readline = require('readline');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const validator = require('validator');
+const chalk = require('chalk');
+// console.log(validator.isEmail('adam@gmail.com'))
+// console.log(validator.isMobilePhone('0812323443', 'id-ID'))
+// console.log(chalk.black.bgRed('Hello warna biru'));
+
+const nama = 'adam';
+const pesan = chalk`Lorem ipsum, dolor {bgRed.green sit amet} consectetur adipisicing elit. Ex, impedit. Nama saya: ${nama}`;
+console.log(pesan)
