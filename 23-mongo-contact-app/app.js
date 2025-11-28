a3768c77ed9d8ca58b5fa90d1374b84ea0398263
Adam Abiyu <advmabiyu@gmail.com>
2025-11-28 23:05:55 +0700

    modif episode 23

--- 23-mongo-contact-app/app.js
+++ 23-mongo-contact-app/app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+
+require('./utils/db')
+const Contact = require('./model/contact')
+
 const app = express();
 const port = 3000;
 
@@ -12,6 +19,16 @@ app.use(expressLayouts);
 app.use(express.static('public'))
 // Built-in middleware buat data yang dikirim harus diparsing dulu
 app.use(express.urlencoded( { extended: true }))
+
+// konfigurasi flash
+app.use(cookieParser('secret'))
+app.use(session({
+  cookie: { maxAge: 6000 },
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(flash());
 
 
 // Halaman Home
@@ -47,13 +64,29 @@ app.get("/about", (req, res) => {
 });
 
 // Halaman contact
-app.get("/contact", (req, res) => {
-  const contacts = loadContact();
+app.get("/contact", async (req, res) => {
+  // Contact.find().then((result) => {
+  //   res.send(contact)
+  // })
+  
+  const contacts = await Contact.find();
+
   res.render("contact", {
-    layout: "layouts/main-layout",
     title: "Halaman Contact",
+    layout: "layouts/main-layout",
     contacts,
     msg: req.flash("msg"),
+  });
+});
+
+// halaman detail kontak
+app.get("/contact/:nama", async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
+
+  res.render("detail", {
+    layout: "layouts/main-layout",
+    title: "Halaman Detail Contact",
+    contact
   });
 });
 
--- 23-mongo-contact-app/utils/db.js
+++ 23-mongo-contact-app/utils/db.js
@@ -5,29 +5,29 @@ mongoose.connect("mongodb://127.0.0.1:27017/wpu", {
     useCreateIndex: true
 });
 
-// Membuat schema
-const Contact = mongoose.model('Contact', {
-    nama: {
-        type: String,
-        required: true
-    },
-    nohp: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-    },
-})
+// // Membuat schema
+// const Contact = mongoose.model('Contact', {
+//     nama: {
+//         type: String,
+//         required: true
+//     },
+//     nohp: {
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//     },
+// })
 
 // Menambah 1 data
-const contact1 = new Contact({
-    nama: 'amil',
-    nohp: '0815817519',
-    email: 'amil@gmail.com'
-})
+// const contact1 = new Contact({
+//     nama: 'amil',
+//     nohp: '0815817519',
+//     email: 'amil@gmail.com'
+// })
 
-// simpan ke collection
-contact1.save().then((contact) => {
-    console.log(contact)
-})
+// // simpan ke collection
+// contact1.save().then((contact) => {
+//     console.log(contact)
+// })
